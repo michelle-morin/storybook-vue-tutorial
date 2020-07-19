25615da382a34b771ed44cccbe64c875a8147026
Michelle Morin <morin2m@gmail.com>
2020-07-18 21:16:28 -0700

    add vuex store and separate tasklist into presentational and container components

--- src/components/PureTaskList.stories.js
+++ src/components/PureTaskList.stories.js
@@ -1,4 +1,4 @@
-import TaskList from "./TaskList";
+import PureTaskList from "./PureTaskList";
 import { taskData, actionsData } from "./Task.stories";
 
 const paddedList = () => {
@@ -27,8 +27,8 @@ export const withPinnedTasksData = [
 
 // default TaskList state
 export const Default = () => ({
-  components: { TaskList },
-  template: `<task-list :tasks="tasks" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
+  components: { PureTaskList },
+  template: `<pure-task-list :tasks="tasks" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
   props: {
     tasks: {
       default: () => defaultTasksData
@@ -38,8 +38,8 @@ export const Default = () => ({
 });
 // tasklist with pinned tasks
 export const WithPinnedTasks = () => ({
-  components: { TaskList },
-  template: `<task-list :tasks="tasks" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
+  components: { PureTaskList },
+  template: `<pure-task-list :tasks="tasks" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
   props: {
     tasks: {
       default: () => withPinnedTasksData
@@ -49,13 +49,13 @@ export const WithPinnedTasks = () => ({
 });
 // tasklist in loading state
 export const Loading = () => ({
-  components: { TaskList },
-  template: `<task-list loading @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
+  components: { PureTaskList },
+  template: `<pure-task-list loading @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
   methods: actionsData
 });
 // tasklist no tasks
 export const Empty = () => ({
-  components: { TaskList },
-  template: `<task-list @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
+  components: { PureTaskList },
+  template: `<pure-task-list @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
   methods: actionsData
 });
--- tests/unit/TaskList.spec.js
+++ tests/unit/TaskList.spec.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
-import TaskList from "../../src/components/TaskList.vue";
+import PureTaskList from "../../src/components/PureTaskList.vue";
 import { withPinnedTasksData } from "../../src/components/TaskList.stories";
 
 it("renders pinned tasks at the start of the list", () => {
-  const Constructor = Vue.extend(TaskList);
+  const Constructor = Vue.extend(PureTaskList);
   const vm = new Constructor({
     propsData: { tasks: withPinnedTasksData }
   }).$mount();
