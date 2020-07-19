b6e7d40dcb842c3d30957e6424db549a013b5f8a
Michelle Morin <morin2m@gmail.com>
2020-07-18 20:47:41 -0700

    remove extra comma

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -14,7 +14,7 @@ export const taskData = {
   id: "1",
   title: "Test Task",
   state: "Task_INBOX",
-  updated_at: new Date(2019, 0, 1, 9, 0),
+  updated_at: new Date(2019, 0, 1, 9, 0)
 };
 
 const taskTemplate = `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`;
